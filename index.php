cb16046d85ff9f0959b9bebda76f63e3f9814cd9
benoitMarie09 <bouduben@gmail>
2023-04-28 14:13:44 +0200

    ajout : traitement du formulaire

--- index.php
+++ index.php
@@ -7,22 +7,30 @@ error_reporting(~0);
 <head>
     <title>Factures</title>
     <meta charset="utf-8" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" async></script>
     <script src="https://cdn.ckeditor.com/4.21.0/standard/ckeditor.js"></script>
 </head>
 <body>
-<?php
-#region === Header ===================================================
-require_once("protected/includes/header.php");
-#endregion
-?>
-    <h1 class="text-center">Prise de note</h1>
 <?php
 #region === Intégration des classes ===========================================
 spl_autoload_register( function ( $class ) {
     require_once( 'protected/classes/' . str_replace( '\\', '/', $class ). '.php' );
     });
 #endregion
+#region === Traitement ===============================================
+// Teste si le formulaire de création a été posté.
+if ( isset( $_REQUEST['id'] )) {
+    // Exécute le traitement des données postées.
+    require( "protected/includes/traitement.php" );
+}
+#endregion
+#region === Header ===================================================
+require_once("protected/includes/header.php");
+#endregion
+?>
+    <h1 class="text-center">Prise de note</h1>
+<?php
 #region === Router ===================================================
 $page = $_REQUEST['page'] ?? 'accueil' ;
 if (! file_exists(  "protected/pages/$page.php" )){
--- protected/classes/Note/Note.php
+++ protected/classes/Note/Note.php
@@ -10,7 +10,7 @@ class Note extends \HtmlElement
     /** @var int primary key de la note. */ 
     private int $id;
     /** @var string Titre de la note. */
-    private string $titre;
+    public string $titre;
     /** @var string Descriptif de la note. */
     private string $descriptif;
     /** @var ?int $idNote foreign key de la note de référence. */
@@ -28,9 +28,30 @@ class Note extends \HtmlElement
     #endregion
 
     #region --- Methodes --------------------------------
+    /**
+     * Récupère la note de référence
+     * @return ?Note note de référence
+     */
+    private function getRef()
+    {
+        if( isset( $this->idNote ) )
+        {
+            $dao = new \DAO\DAONote();
+            return $dao->get( $this->idNote );
+        }
+        return null;
+
+    }
+
     public function rendre( $nivIndent )
     {
-        $html = self::indente( $nivIndent )."<td class=\"text-center\">$this->titre</td>";
+        $html = self::indente( $nivIndent )."<td id=\"note_$this->id\" class=\"text-center\"  data-toggle=\"collapse\" data-target=\"#$this->id\">"
+                ."$this->titre <br>" 
+                .(isset( $this->idNote ) ? '<span class="blockquote-footer">'."Ref $this->idNote : "."<a href=\"#note_$this->idNote \">".$this->getRef()->titre.'</a>' .'</span>': '')
+                ."</td>";
+        $html .= self::indente( $nivIndent )."<div id=\"$this->id\" class=\"collapse\">"
+                .   self::indente( $nivIndent +1 ).$this->descriptif
+                .self::indente( $nivIndent )."</div>";
         return $html;
     }
     #endregion
