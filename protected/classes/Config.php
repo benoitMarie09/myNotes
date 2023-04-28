e602b7a73eacd7632bff24b1ada0b6c8e1cb56fd
benoitMarie09 <bouduben@gmail>
2023-04-28 09:00:34 +0200

    ajout gitignore

--- protected/classes/Config.php
+++ protected/classes/Config.php
@@ -3,6 +3,6 @@ class Config
 {
     const BDD_SERVEUR = '127.0.0.1';
     const BDD_USER = 'ben';
-    const BDD_PASS = 'Bouducon31';
+    const BDD_PASS = 'ben';
     const BDD_BASE = 'prise_note';
 }
